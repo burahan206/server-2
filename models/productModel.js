32bace149c44e5c5e850349b96a100504b557b84
BLUE biyayeetahe <burahan206@gmail.com>
2024-12-02 13:46:40 +0700

    update 13.46

--- models/productModel.js
+++ models/productModel.js
@@ -4,7 +4,7 @@ const productSchema =new mongoose.Schema({
     name: { type: String, require: true},
     description :{ type: String, require: true},
     price: { type: Number, require: true},
-    Image: { type: Array, require: true},
+    image: { type: Array, require: true},
     category: { type: String, require: true},
     subCategory: { type: String, require: true},
     sizes: { type: Array, require: true},
